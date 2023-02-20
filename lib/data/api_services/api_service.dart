966816429e167402eae01db198b4ff464e87d622
Odilbek <odilbekjuraev07@gmail.com>
2023-02-20 15:31:14 +0500

    some changes done

--- lib/data/api_services/api_service.dart
+++ lib/data/api_services/api_service.dart
@@ -4,19 +4,26 @@ import 'package:foods_app_api/data/models/food_model/food_model.dart';
 import 'package:foods_app_api/data/models/my_response/my_response.dart';
 
 class ApiService extends ApiClient {
-  Future<MyResponse> getFoodsData() async {
+  Future<MyResponse> getFoodsData({
+    String health = '',
+    String nutritionType = '',
+    String category = '',
+  }) async {
     MyResponse myResponse = MyResponse(error: "");
     try {
       Response response =
           await dio.get("${dio.options.baseUrl}/v2/parser", queryParameters: {
         'app_id': "05d387a1",
         'app_key': 'fce49db6c7b785162e3a3809b834f72a',
+        'health': health == '' ? 'vegan' : health,
+        'nutritionType': nutritionType == '' ? 'cooking' : nutritionType,
+        'category': category == '' ? 'generic-foods' : category,
       });
       if (response.statusCode == 200) {
-        myResponse.data = (response.data as List?)
-                ?.map((e) => FoodData.fromJson(e))
-                .toList() ??
-            [];
+        print('statusCode ${response.statusCode}');
+        myResponse.data =
+            FoodData.fromJson(response.data as Map<String, dynamic>);
+        print('statusCode ${myResponse.data}');
       }
     } catch (err) {
       myResponse.error = err.toString();
